a4549dad94135f4a8cd17ccb66d2b8239ba7d3bc
AbylaiShazhabayev <112696224+AbylaiShazhabayev@users.noreply.github.com>
2022-11-20 20:53:17 +0600

    Delete secure directory

--- secure/db/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   // The DB credentials should not be be stored in source control, but they are include here to provide a complete example.  
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'sql_injection');
-   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   $db->set_charset("utf8");
-?>
