3c5622d5e75c80c22e2bdf1491c5862298c5baa0
AbylaiShazhabayev <112696224+AbylaiShazhabayev@users.noreply.github.com>
2022-11-20 20:45:26 +0600

    Delete db directory

--- db/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'sql_injection');
-   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   $db->set_charset("utf8");
-?>
